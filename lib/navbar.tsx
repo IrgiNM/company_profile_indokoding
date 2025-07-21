4c50314e202ed11d76025b63dfc7efae37b610bf
unknown <irginazwamustofa@gmail.com>
2025-07-21 12:30:53 +0700

    benerin mapping navbar

--- lib/navbar.tsx
+++ lib/navbar.tsx
@@ -1,55 +1,68 @@
 "use client";
 import NavLink from '@/components/navLink'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar() {
+    const [bgNav, setBgNav] = useState("Home");
     const nav = [
         {
             isi: "Home",
             warna: "text-[#4F006C]",
-            bg: "bg-[#F0D4FF] hover:bg-[#D9C6FF] active:bg-[#BFA4FF]",
+            active: "text-[#4F006C] bg-[#D9C6FF]",
+            bg: "hover:bg-[#D9C6FF] hover:font-bold active:bg-[#BFA4FF]",
             link: ()=>(
-              console.log("Home clicked")   
+              console.log("Home clicked"),
+              setBgNav("Home")   
             )
         },
         {
             isi: "Our Work",
             warna: "text-[#128900]",
-            bg: "hover:bg-[#D4FFDB] active:bg-[#BFA4FF]",
+            active: "text-[#128900] bg-[#D4FFDB]",
+            bg: "hover:bg-[#D4FFDB] hover:font-bold active:bg-[#BFA4FF]",
             link: ()=>(
-              console.log("Home clicked")   
+              console.log("Our Work clicked"),
+              setBgNav("Our Work")   
             )
         },
         {
             isi: "About Us",
             warna: "text-[#004C6C]",
-            bg: "hover:bg-[#D4F6FF] active:bg-[#BFA4FF]",
+            active: "text-[#004C6C] bg-[#D4F6FF]",
+            bg: "hover:bg-[#D4F6FF] hover:font-bold active:bg-[#BFA4FF]",
             link: ()=>(
-              console.log("Home clicked")   
+              console.log("About Us clicked"),
+              setBgNav("About Us")   
             )
         },
         {
             isi: "Contact",
             warna: "text-[#6C4E00]",
-            bg: "hover:bg-[#FEFFD4] active:bg-[#BFA4FF]",
+            active: "text-[#6C4E00] bg-[#FEFFD4]",
+            bg: "hover:bg-[#FEFFD4] hover:font-bold active:bg-[#BFA4FF]",
             link: ()=>(
-              console.log("Home clicked")   
+              console.log("Contact clicked"),
+              setBgNav("Contact")   
             )
         },
         {
             isi: "Career",
             warna: "text-[#004C6C]",
-            bg: "hover:bg-[#D4F6FF] active:bg-[#BFA4FF]",
+            active: "text-[#004C6C] bg-[#D4F6FF]",
+            bg: "hover:bg-[#D4F6FF] hover:font-bold active:bg-[#BFA4FF]",
             link: ()=>(
-              console.log("Home clicked")   
+              console.log("Career clicked"),
+              setBgNav("Career")   
             )
         },
         {
             isi: "Book Online",
             warna: "text-[#4F006C]",
-            bg: "hover:bg-[#D9C6FF] active:bg-[#BFA4FF]",
+            active: "text-[#4F006C] bg-[#D9C6FF]",
+            bg: "hover:bg-[#D9C6FF] hover:font-bold active:bg-[#BFA4FF]",
             link: ()=>(
-              console.log("Home clicked")   
+              console.log("Book Online clicked"),
+              setBgNav("Book Online")   
             )
         },
     ]
