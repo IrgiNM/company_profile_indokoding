703a2dfadfd53ce46300a8034ebf7c67c02d182b
unknown <irginazwamustofa@gmail.com>
2025-07-22 12:51:40 +0700

    coba lagi

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Home from '@/lib/home'
 import Navbar from '@/lib/navbar'
 import React from 'react'
 import Footer from '@/components/footer'
+import OurWork from '@/lib/ourWork'
 
 export default function page() {
   return (
     <div className='w-full flex flex-col items-center justify-center overflow-hidden'>
-      <Navbar />
-      <Home />
+      <Navbar/>
+      <Home/>
+      <OurWork/>
         <Footer />
     </div>
   )
---
+++ lib/ourWork.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function OurWork() {
+  return (
+    <div className='w-full relative flex flex-col items-end justify-center'>
+        <h1 className='text-4xl text-[#128900] font-extrabold mr-50'>Our Work</h1>
+        <p className='mr-50'>A collection of website we've made with passion</p>
+    </div>
+  )
+}
